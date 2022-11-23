57b1b7f1078f80a3b722c42ba58c8c9b8a789d9f
Ramonrz <ramonderezende@gmail.com>
2022-11-23 17:19:34 -0300

    Add files via upload

---
+++ assets/js/listMenu.js
@@ -0,0 +1,170 @@
+let listMenu = [
+    {
+        id: 1,
+        name: "Picanha Montada",
+        price: 12.0,
+        qtd: 0
+    },
+    {
+        id: 2,
+        name: "Contra Filé",
+        price: 10.0,
+        qtd: 0
+    },
+    {
+        id: 3,
+        name: "Carne de Sol",
+        price: 10.0,
+        qtd: 0
+    },
+    {
+        id: 4,
+        name: "Kafta Libanesa",
+        price: 9.0,
+        qtd: 0
+    },
+    {
+        id: 5,
+        name: "Frango C/ Bacon",
+        price: 10.0,
+        qtd: 0
+    },
+    {
+        id: 6,
+        name: "Frango Filé de Peito",
+        price: 8.0,
+        qtd: 0
+    },
+    {
+        id: 7,
+        name: "Coração de Frango",
+        price: 8.0,
+        qtd: 0
+    },
+    {
+        id: 8,
+        name: "Linguiça Suína Fina Ap",
+        price: 8.0,
+        qtd: 0
+    },
+    {
+        id: 9,
+        name: "Pão C/ Alho",
+        price: 7.0,
+        qtd: 0
+    },
+    {
+        id: 10,
+        name: "Jantinha",
+        price: 12.0,
+        qtd: 0
+    },
+    {
+        id: 11,
+        name: "Feijão Tropeiro",
+        price: 8.0,
+        qtd: 0
+    },
+    {
+        id: 12,
+        name: "Mandioca",
+        price: 7.0,
+        qtd: 0
+    },
+    {
+        id: 13,
+        name: "Caldo Ervilha C/ Frango",
+        price: 15.0,
+        qtd: 0
+    },
+    {
+        id: 14,
+        name: "Caldo Verde",
+        price: 15.0,
+        qtd: 0
+    },
+    {
+        id: 15,
+        name: "Caldo Vaca Atolada",
+        price: 15.0,
+        qtd: 0
+    },
+    {
+        id: 16,
+        name: "Porção Pastéis Carne",
+        price: 26.99,
+        qtd: 0
+    },
+    {
+        id: 17,
+        name: "Porção Pastéis Carne/Queijo",
+        price: 26.99,
+        qtd: 0
+    },
+    {
+        id: 18,
+        name: "Porção Kibe",
+        price: 23.99,
+        qtd: 0
+    },
+    {
+        id: 19,
+        name: "Porção Coxinha",
+        price: 23.99,
+        qtd: 0
+    },
+    {
+        id: 20,
+        name: "Porção Churros",
+        price: 23.99,
+        qtd: 0
+    },
+    {
+        id: 28,
+        name: "Discos Carne C/ Quejo",
+        price: 26.99,
+        qtd: 0
+    },
+    {
+        id: 21,
+        name: "Suco Laranja (500ml)",
+        price: 10.50,
+        qtd: 0
+    },
+    {
+        id: 22,
+        name: "Coca-Cola (600ml)",
+        price: 7.50,
+        qtd: 0
+    },
+    {
+        id: 23,
+        name: "Coca-Cola (310ml)",
+        price: 5.0,
+        qtd: 0
+    },
+    {
+        id: 24,
+        name: "Coca Cola S/ Açúcar (310ml)",
+        price: 5.0,
+        qtd: 0
+    },
+    {
+        id: 25,
+        name: "Guaraná Antarctica (269ml)",
+        price: 5.0,
+        qtd: 0
+    },
+    {
+        id: 26,
+        name: "Feiojoada",
+        price: 22.90,
+        qtd: 0
+    },
+    {
+        id: 27,
+        name: "Filé Molho Madeira",
+        price: 24.90,
+        qtd: 0
+    }
+];
